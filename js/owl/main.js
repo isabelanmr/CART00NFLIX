9246076680485ded3ffc70cc2da236ae24e4f059
isabelanmr <80434214+isabelanmr@users.noreply.github.com>
2021-06-23 09:57:33 -0300

    Delete main.js

--- js/owl/main.js
+++
@@ -1,7 +0,0 @@
-function clicou(){
-    document.getElementById("trailer").innerHTML = "<b>Clique aqui para ver o trailer<b>";
-}
-
-function redirecionar(){
-    window.open("https://www.youtube.com/watch?v=9ikp_fFYb-w/");
-}
