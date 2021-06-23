0ee13e481a7c23937e5964bf848bbe8ccfd23128
isabelanmr <80434214+isabelanmr@users.noreply.github.com>
2021-06-22 23:57:11 -0300

    Add files via upload

---
+++ js/owl/main.js
@@ -0,0 +1,7 @@
+function clicou(){
+    document.getElementById("trailer").innerHTML = "<b>Clique aqui para ver o trailer<b>";
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=9ikp_fFYb-w/");
+}
